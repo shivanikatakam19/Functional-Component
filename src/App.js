69ac5985ecfd81316f2fb8a4fd8749146163bd4d
thrinathNarhari <106737124+shivanikatakam19@users.noreply.github.com>
2024-07-25 10:59:32 +0530

    added class component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import Content from './components/content';
 import Footer from './components/footer';
 import Navbar from './components/navbar';
+import ClassComponent from './components/classComponent';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Content />
+      <ClassComponent />
       <Footer />
     </div>
   );
